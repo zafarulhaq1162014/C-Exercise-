626d8be18ec6db4b1268435ef2fed34b39a7ebab
zafar <134407621+zafarulhaq1162014@users.noreply.github.com>
2023-05-23 21:53:39 +0500

    Question :
    You have to write a C++ program that will print ASCII code of first 5 characters on the screen.

---
+++ Task4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"The ASCII value of [a] is :: "<<int('a')<<endl;
+    cout<<"The ASCII value of [a] is :: "<<int('b')<<endl;
+    cout<<"The ASCII value of [a] is :: "<<int('c')<<endl;
+    cout<<"The ASCII value of [a] is :: "<<int('d')<<endl;
+    cout<<"The ASCII value of [a] is :: "<<int('e')<<endl;
+    return 0;
+}
