cf818e9a4db122dc203d6658d082bec129fbc85d
zafar <134407621+zafarulhaq1162014@users.noreply.github.com>
2023-05-23 21:47:22 +0500

    Basic Question
    You have to write a C++ program that will print your complete bio data on the screen.
    Output:
    Name:
    University ID:
    Father Name:
    Phone Number:
    Address:
    Semester:
    Course:

---
+++ Task3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Name: Zafar \n"
+<<"University ID: L1f22bscs0193 \n"
+<<"Father Name:  Ishaq \n"
+<<"Phone Number: 012458 \n"
+<<"Address: Lahore \n"
+<<"Semester: 1st \n"
+<<"Course: Bscs \n";
+    return 0;
+}
