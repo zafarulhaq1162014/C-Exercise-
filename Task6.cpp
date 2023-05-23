43e3faddebc119270c1e368ad0bd8ff39834aa90
zafar <134407621+zafarulhaq1162014@users.noreply.github.com>
2023-05-23 22:08:17 +0500

    Question
    Write a C++ program to print the table of 5 on console using Cout statement as shown below:

---
+++ Task6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<" 5  * 1  = "<< 5*1<<endl;
+    cout<<" 5  * 2  = "<< 5*2<<endl;
+    cout<<" 5  * 3  = "<< 5*3<<endl;
+    cout<<" 5  * 4  = "<< 5*4<<endl;
+    cout<<" 5  * 5  = "<< 5*5<<endl;
+    cout<<" 5  * 6  = "<< 5*6<<endl;
+    cout<<" 5  * 7  = "<< 5*7<<endl;
+    cout<<" 5  * 8  = "<< 5*8<<endl;
+    cout<<" 5  * 9  = "<< 5*9<<endl;
+    cout<<" 5  * 10  = "<< 5*10<<endl;
+    return 0;
+}
