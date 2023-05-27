3a08c8fed27db947347f545edc361bdb4d79525a
zafar <134407621+zafarulhaq1162014@users.noreply.github.com>
2023-05-27 20:05:49 +0500

    Reference & value
    pass by reference and pass by value

---
+++ Task13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void update(int x){
+    x++;
+}
+void update1(int& x){
+    x++;
+}
+int main(){
+    int a=5;
+    update(a);
+    cout<<a<<endl;//after updade by value
+    update(a);
+    cout<<a<<endl;//after updade by sending pass by reference 
+
+    return 0;
+}
